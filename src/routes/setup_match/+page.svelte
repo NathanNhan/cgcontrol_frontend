5ad5588291da42447bb43004b68f3ff2dc42adea
NathanNhan <trongnhan8150@gmail.com>
2024-06-02 18:02:29 -0700

    update 02/06/2024

--- src/routes/setup_match/+page.svelte
+++ src/routes/setup_match/+page.svelte
@@ -8,4 +8,8 @@
 
 <FormSetup type="home" />
 <FormSetup type="away"/>
+
 </div>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto mt-5">
+    Setup match
+</button>
